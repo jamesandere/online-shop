1ba372f084cdfa82fd3146e5945e6f3f120187a6
jamesandere <jamesandere56@gmail.com>
2022-04-22 17:08:48 +0300

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      hello
+    </div>
+  );
+}
+
+export default App;
