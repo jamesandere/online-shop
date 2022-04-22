53616da6b6fc7ab00c610ecd6d3384efbbc564b3
jamesandere <jamesandere56@gmail.com>
2022-04-22 22:22:27 +0300

    Adding api middleware

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import productsReducer, {productsFetch} from './productsSlice';
+import { productsApi } from './productsApi';
 
 export const store = configureStore({
     reducer: {
-        products: productsReducer, 
-    }
+        products: productsReducer,
+        [productsApi.reducerPath]: productsApi.reducer, 
+    },
+    middleware: (getDefaultMiddleware) => 
+        getDefaultMiddleware().concat(productsApi.middleware)
 })
 
 store.dispatch(productsFetch());
