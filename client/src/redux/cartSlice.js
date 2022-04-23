d1a5cabefedf7648e46921745dbe4a99f7c06aa5
jamesandere <jamesandere56@gmail.com>
2022-04-23 11:54:14 +0300

    Create add to cart action

--- client/src/redux/cartSlice.js
+++ client/src/redux/cartSlice.js
@@ -1,7 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { toast } from 'react-toastify';
 
 const initialState = {
-    cartItems: [],
+    cartItems: localStorage.getItem('cartItems') ?
+     JSON.parse(localStorage.getItem('cartItems')) : [],
     cartTotalQuantity: 0,
     cartTotalAmount: 0,
 }
@@ -9,7 +11,27 @@ const initialState = {
 const cartSlice = createSlice({
     name: 'cart',
     initialState,
-    reducers: {}
+    reducers: {
+        addToCart(state,action){
+            const itemIndex = state.cartItems.findIndex(item => item.id === action.payload.id);
+
+            if(itemIndex >= 0){
+                state.cartItems[itemIndex].quantity += 1;
+                toast.info(`Increased ${state.cartItems[itemIndex].name} cart quantity`, {
+                    position: "bottom-left"
+                })
+            } else {
+                const tempProduct = {...action.payload, quantity: 1};
+            state.cartItems.push(tempProduct);
+            toast.success(`${action.payload.name} added to cart`, {
+                position: "bottom-left"
+            })
+            }
+
+            localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+        }
+    }
 })
 
+export const {addToCart} = cartSlice.actions;
 export default cartSlice.reducer;
