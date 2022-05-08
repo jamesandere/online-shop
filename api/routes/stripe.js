ebfffe388bda522c93a57da160970d6ab6cb055d
jamesandere <jamesandere56@gmail.com>
2022-05-08 13:11:53 +0300

    Discharge cart items to stripe

--- api/routes/stripe.js
+++ api/routes/stripe.js
@@ -6,19 +6,26 @@ require("dotenv").config();
 const stripe = Stripe(process.env.STRIPE_KEY);
 
 router.post('/create-checkout-session', async (req, res) => {
+    const line_items = req.body.cartItems.map((item) => {
+        return {
+            price_data: {
+                currency: 'usd',
+                product_data: {
+                  name: item.name,
+                  images: [item.image],
+                  description: item.desc,
+                  metadata: {
+                      id: item.id
+                  },
+                },
+                unit_amount: 2000,
+              },
+              quantity: 1,
+        }
+    })
+
     const session = await stripe.checkout.sessions.create({
-      line_items: [
-        {
-          price_data: {
-            currency: 'usd',
-            product_data: {
-              name: 'T-shirt',
-            },
-            unit_amount: 2000,
-          },
-          quantity: 1,
-        },
-      ],
+      line_items,
       mode: 'payment',
       success_url: `${process.env.CLIENT_URL}/checkout-success`,
       cancel_url: `${process.env.CLIENT_URL}/cart`,
