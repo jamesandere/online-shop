ac9501135227df6ce8aee00fe2bb05eacd589389
jamesandere <jamesandere56@gmail.com>
2022-05-14 22:30:40 +0300

    Create product routes

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,8 +1,44 @@
 const router = require('express').Router();
-const products = require('../data');
+// const products = require('../data');
+const cloudinary = require('../utils/cloudinary');
+const Product = require("../models/products");
 
-router.get('/', (req, res) => {
-    res.send(products)
+router.post("/", async (req, res) => {
+    const { name, brand, desc, price, image } = req.body;
+
+    try {
+        if(image){
+            const uploadRes = await cloudinary.uploader.upload(image, {
+                upload_preset: "shopee"
+            })
+
+            if(uploadRes){
+                const product = new Product({
+                    name,
+                    brand,
+                    desc,
+                    price,
+                    image: uploadRes
+                });
+
+                const savedProduct = await product.save();
+                res.status(200).send(savedProduct);
+            }
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
+})
+
+router.get('/', async(req, res) => {
+    try {
+        const products = await Product.find();
+        res.status(200).send(products);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
 })
 
 module.exports = router;
