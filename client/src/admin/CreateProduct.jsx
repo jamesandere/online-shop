e44c166a818851fc0a75d5609523cf9435bfad6a
jamesandere <jamesandere56@gmail.com>
2022-05-14 22:59:03 +0300

    Create products form

--- client/src/admin/CreateProduct.jsx
+++ client/src/admin/CreateProduct.jsx
@@ -1,7 +1,111 @@
+import { useState } from "react";
+import styled from "styled-components";
+import { useDispatch } from "react-redux";
+
 const CreateProduct = () => {
+  const [productImg, setProductImg] = useState("");
+  const [brand, setBrand] = useState("");
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [desc, setDesc] = useState("");
+
+  const handleImageUpload = (e) => {
+    const file = e.target.files[0];
+
+    TransformFile(file);
+  }
+
+  const TransformFile = (file) => {
+    const reader = new FileReader();
+
+    if(file){
+      reader.readAsDataURL(file);
+      reader.onloadend = () => {
+        setProductImg(reader.result);
+      }
+    } else {
+      setProductImg("");
+    }
+  }
+
   return (
-    <div>CreateProduct</div>
+    <StyledCreateProduct>
+      <StyledForm>
+        <h3>Create a Product</h3>
+        <input type="file" accept="image/*" 
+        onChange={handleImageUpload} required/>
+        <select onChange={(e) => setBrand(e.target.value)} required>
+          <option value="">Select Brand</option>
+          <option value="iphone">Apple</option>
+          <option value="samsung">Samsung</option>
+          <option value="xiaomi">Xiaomi</option>
+          <option value="other">Other</option>
+        </select>
+        <input
+          type="text"
+          placeholder="Name"
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+        <input
+          type="number"
+          placeholder="Price"
+          onChange={(e) => setPrice(e.target.value)}
+          required
+        />
+        <input
+          type="text"
+          placeholder="Short Description"
+          onChange={(e) => setDesc(e.target.value)}
+          required
+        />
+      </StyledForm>
+      <ImagePreview></ImagePreview>
+    </StyledCreateProduct>
   )
 }
 
-export default CreateProduct
+export default CreateProduct;
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  max-width: 300px;
+  margin-top: 2rem;
+  select,
+  input {
+    padding: 7px;
+    min-height: 30px;
+    outline: none;
+    border-radius: 5px;
+    border: 1px solid rgb(182, 182, 182);
+    margin: 0.3rem 0;
+    &:focus {
+      border: 2px solid #fca311;
+    }
+  }
+  select {
+    color: rgb(95, 95, 95);
+  }
+`;
+
+const StyledCreateProduct = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ImagePreview = styled.div`
+  margin: 2rem 0 2rem 2rem;
+  padding: 2rem;
+  border: 1px solid rgb(183, 183, 183);
+  max-width: 300px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 2rem;
+  color: rgb(78, 78, 78);
+  img {
+    max-width: 100%;
+  }
+`;
--- client/src/admin/Products.jsx
+++ client/src/admin/Products.jsx
@@ -1,11 +1,14 @@
 import { useNavigate, Outlet } from "react-router-dom";
+import { AdminHeaders, PrimaryButton } from "./CommonStyled";
 
 const Products = () => {
     const navigate = useNavigate(); 
   return (
     <>
+    <AdminHeaders>
     <h3>Products</h3>
-    <button onClick={()=> navigate("/admin/products/create-product")}>Create</button>
+    <PrimaryButton onClick={()=> navigate("/admin/products/create-product")}>Create</PrimaryButton>
+    </AdminHeaders>
     <Outlet />
     </>
   )
