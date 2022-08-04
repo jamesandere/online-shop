04da1fed5cd06a4628f25c947a654ac2f7d17acb
jamesandere <jamesandere56@gmail.com>
2022-08-04 15:30:05 +0300

    Dispatch delete product action creator

--- client/src/admin/list/ProductsList.jsx
+++ client/src/admin/list/ProductsList.jsx
@@ -1,9 +1,13 @@
 import styled from "styled-components";
 import { DataGrid } from "@mui/x-data-grid";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { productsDelete } from "../../redux/productsSlice";
 
 export default function ProductsList() {
   const { items } = useSelector((state) => state.products);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const rows =
     items &&
@@ -50,13 +54,19 @@ export default function ProductsList() {
       renderCell: (params) => {
         return (
           <Actions>
-            <Delete>Delete</Delete>
-            <View>View</View>
+            <Delete onClick={()=> handleDelete(params.row.id)}>Delete</Delete>
+            <View onClick={()=> navigate(`/product/${params.row.id}`)}>View</View>
           </Actions>
         );
       },
     },
   ];
+
+  const handleDelete = (id) => {
+    dispatch(
+      productsDelete(id)
+    );
+  };
 
   return (
     <div style={{ height: 400, width: "100%" }}>
@@ -66,6 +76,7 @@ export default function ProductsList() {
         pageSize={5}
         rowsPerPageOptions={[5]}
         checkboxSelection
+        disableSelectionOnClick
       />
     </div>
   );
