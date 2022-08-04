2e453c2f92dffbc5ff669a6c111781c9676d5482
jamesandere <jamesandere56@gmail.com>
2022-08-04 15:25:49 +0300

    Create delete product and fetch single product endpoints

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,44 +1,81 @@
-const router = require('express').Router();
-const cloudinary = require('../utils/cloudinary');
+const router = require("express").Router();
+const cloudinary = require("../utils/cloudinary");
 const Product = require("../models/products");
-const { auth, isAdmin } = require('../middleware/auth');
+const { auth, isAdmin } = require("../middleware/auth");
 
 router.post("/", isAdmin, async (req, res) => {
-    const { name, brand, desc, price, image } = req.body;
+  const { name, brand, desc, price, image } = req.body;
 
-    try {
-        if(image){
-            const uploadRes = await cloudinary.uploader.upload(image, {
-                upload_preset: "shopee"
-            })
+  try {
+    if (image) {
+      const uploadRes = await cloudinary.uploader.upload(image, {
+        upload_preset: "shopee",
+      });
 
-            if(uploadRes){
-                const product = new Product({
-                    name,
-                    brand,
-                    desc,
-                    price,
-                    image: uploadRes
-                });
+      if (uploadRes) {
+        const product = new Product({
+          name,
+          brand,
+          desc,
+          price,
+          image: uploadRes,
+        });
 
-                const savedProduct = await product.save();
-                res.status(200).send(savedProduct);
-            }
-        }
-    } catch (error) {
-        console.log(error);
-        res.status(500).send(error);
+        const savedProduct = await product.save();
+        res.status(200).send(savedProduct);
+      }
     }
-})
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
 
-router.get('/', async(req, res) => {
-    try {
-        const products = await Product.find();
-        res.status(200).send(products);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send(error);
+router.get("/", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).send(products);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
+// GET PRODUCT
+
+router.get("/find/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.status(200).send(product);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// DELETE PRODUCT
+
+router.delete("/:id", isAdmin, async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) return res.status(404).send("Product not found...");
+
+    if (product.image.public_id) {
+      const destroyedResponse = await cloudinary.uploader.destroy(
+        product.image.public_id
+      );
+
+      if (destroyedResponse) {
+        const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+
+        res.status(200).send(deletedProduct);
+      }
+    } else {
+      console.log("Action terminated. Failed to delete product image");
     }
-})
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
 module.exports = router;
